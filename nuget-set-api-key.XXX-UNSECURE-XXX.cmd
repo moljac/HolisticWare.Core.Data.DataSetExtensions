0972b7c1bb33e25d086fb8c5a291e2976cccb8c4
moljac <mcvjetko@holisticware.net>
2015-05-06 11:33:38 +0200

    unsecure script samples added -  tampltes for secure ones

---
+++ nuget-set-api-key.XXX-UNSECURE-XXX.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+:: 	DO NOT PUT YOUR KEY in HERE
+::	add *.secure.cmd to .gitingore
+::
+::	copy this file to
+:: 		nuget-set-api-key.secure.cmd
+::	which will be called by scripts
+
+set NUGET=.nuget\nuget
+
+set API_KEY="Users NuGet API Key"
+
+::========================================================================================
+:: 	nuget setapikey <API key> [options]
+::
+:: 	Specify the API key to save and an optional URL to the server that provided the API key.
+:: 	
+:: 	SetApiKey 
+::		Command Options
+:: 	Source	
+::		Server URL where the API key is valid.
+:: 	Help	
+::		help
+:: 	Verbosity	
+:		Display this amount of details in the output: normal, quiet, (v2.5) detailed.
+:: 	NonInteractive	
+::		Do not prompt for user input or confirmations.
+:: 	ConfigFile	
+::		(v2.5) The NuGet configuation file. 
+::		If not specified, file %AppData%\NuGet\NuGet.config is used as configuration file.
+
+echo API_KEY = %API_KEY%
+%NUGET% ^
+	setapikey ^
+	%API_KEY%
+	
+::========================================================================================
+
+@IF %ERRORLEVEL% NEQ 0 PAUSE	
